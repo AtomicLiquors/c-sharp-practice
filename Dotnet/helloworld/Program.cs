870ef24665e53dc0044397f5a35b6c0257d84496
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-06-15 18:44:22 +0900

    Blazor App init

--- Dotnet/helloworld/Program.cs
+++ Dotnet/helloworld/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// This is a simple C# console application that prints "Hello, World!" to the console.  ~
